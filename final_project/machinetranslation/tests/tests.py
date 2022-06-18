7e2cc31a0ae94e9c05114b9ea88ff90c8a7b0c01
suppaDeo <oehlmannt@gmail.com>
2022-06-18 19:44:30 +0200

    bla

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,19 @@
+import unittest
+from translator import english_to_french, french_to_english
+
+print("test)")
+
+class Test_english_to_french(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(english_to_french("Hello"), "Bonjour")
+    def test2(self):
+        self.assertIsNone(english_to_french(None))
+
+class Test_french_to_english(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(french_to_english("Bonjour"), "Hello")
+    def test2(self):
+        self.assertIsNone(french_to_english(None))
+
+
+unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,10 @@
-import json
-from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import iam_authenticator
+"""
+This module can translate text from French to English and vice versa
+using IBM Watson
+"""
 import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,4 +12,38 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-print("Test")
+authenticator = IAMAuthenticator(apikey)
+
+translator = LanguageTranslatorV3(
+    version="2018-05-01",
+    authenticator=authenticator
+)
+translator.set_service_url(url)
+
+def english_to_french(english_text):
+    """
+    This function takes an english words/sentences as input and returns a
+    french translations
+    """
+    if english_text == "":
+        french_text = ""
+    elif english_text is None:
+        french_text = None
+    else:
+        french_text = translator.translate(english_text,
+        model_id="en-fr").get_result()['translations'][0]['translation']
+    return french_text
+
+def french_to_english(french_text):
+    """
+    This function takes a French words/sentences as input and returns an
+    English translations
+    """
+    if french_text == "":
+        english_text = ""
+    elif french_text is None:
+        english_text = None
+    else:
+        english_text = translator.translate(french_text,
+        model_id="fr-en").get_result()['translations'][0]['translation']
+    return english_text
